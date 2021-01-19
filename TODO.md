46ff672adbcb27bb81e6f303a9d52619c1e1aa86
ZRELOLERZ <batmanbane4@gmail.com>
2021-01-18 22:28:31 -0400

    Some kind of commit

---
+++ TODO.md
@@ -0,0 +1,2 @@
+[ ] dark mode
+[ ] dynamically change the is-active on nav-bar according to section in viewport
---
+++ scripts/script.js
@@ -0,0 +1,54 @@
+function closeImageModal() {
+  var modal = document.getElementById("image-modal");
+  modal.classList.remove("is-active");
+}
+
+let clickedImagesElementsArray = document.getElementsByClassName(
+  "clickable-figure"
+);
+
+for (var i = 0; i < clickedImagesElementsArray.length; i++) {
+  clickedImagesElementsArray[i].addEventListener(
+    "click",
+    function (event) {
+      var imageName = this.children["0"].getAttribute("src");
+      var toSet = document.getElementById("modal-image-placeholder");
+      toSet.setAttribute("src", imageName);
+
+      //set modal to active
+      document.getElementById("image-modal").classList.add("is-active");
+
+      console.log(toSet);
+    },
+    false
+  );
+}
+
+let activeSection = "main";
+let activeSectionElement = document.getElementById(activeSection);
+let navbarItemsArray = document.getElementsByClassName("navbar-item");
+
+for (var i = 0; i < navbarItemsArray.length; i++) {
+  navbarItemsArray[i].addEventListener("click", function (event) {
+    let currentActive = document.getElementsByClassName("is-active")[0];
+    currentActive.classList.remove("is-active");
+    this.classList.add("is-active");
+    currentActive = document.getElementsByClassName("is-active")[0].getAttribute("href");
+    activeSection = currentActive.substring(1);
+    activeSectionElement = document.getElementById(activeSection);
+    console.log(activeSection);
+    console.log(activeSectionElement.getBoundingClientRect());
+  });
+}
+
+function isInViewPort() {
+  var sectionRects = [...document.querySelectorAll('.section')]
+    .map(el => el.getBoundingClientRect().y);
+  console.log(sectionRects);
+}
+
+var cW = document.documentElement.clientWidth;
+var cH = document.documentElement.clientHeight;
+document.addEventListener('scroll', function() {
+  isInViewPort();
+});
